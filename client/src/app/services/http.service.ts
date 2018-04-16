4f0b9940d11f8e90348e73792966b0f6832a3d7e
Erin Bleiweiss <erinbleiweiss@gmail.com>
2018-04-16 01:46:20 -0500

    added new service

---
+++ client/src/app/services/http.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+const HOSTNAME = "";
+
+@Injectable()
+export class HttpService {
+
+  constructor(private http: Http) { }
+
+
+
+}
